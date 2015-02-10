cc5942efc418843e8c408cfbf04154167f439cfd
chesley7 <chesley7@gmail.com>
2015-02-10 00:43:30 -0500

    initial commit from local git

---
+++ NCAA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NCAA
+//
+//  Created by Chesley on 2/10/15.
+//  Copyright (c) 2015 Chesley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
